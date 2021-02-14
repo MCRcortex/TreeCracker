72e21d561afbf5580a52a57372be98293e7e70d2
mcrcortex <18544518+MCRcortex@users.noreply.github.com>
2021-02-14 16:41:32 +1000

    Small cleanup

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,16 +6,6 @@ import me.cortex.TreeCracker.LCG.DiscreteLog;
 
 public class Test {
     public static void main(String[] args) {
-        ChunkRand chunkRand = new ChunkRand();
-        long seed = chunkRand.setPopulationSeed(1234,-34 * 16,43 * 16, MCVersion.v1_16_4);
-        long decoSeed = chunkRand.setDecoratorSeed(seed, 1, 8, MCVersion.v1_16_4);
 
-        JRand rand = JRand.ofScrambledSeed(decoSeed);
-        System.out.println(rand.getSeed());
-
-
-
-        System.out.println(DiscreteLog.distanceFromZero(LCG.JAVA.nextSeed(0)));
-        System.out.println(kaptainwutax.seedutils.lcg.DiscreteLog.distanceFromZero(LCG.JAVA,LCG.JAVA.nextSeed(0)));
     }
 }
--- src/main/java/me/cortex/TreeCracker/Main.java
+++ src/main/java/me/cortex/TreeCracker/Main.java
@@ -1,5 +1,8 @@
 package me.cortex.TreeCracker;
 
+import kaptainwutax.seedutils.lcg.rand.JRand;
+import kaptainwutax.seedutils.mc.ChunkRand;
+import kaptainwutax.seedutils.mc.MCVersion;
 import me.cortex.TreeCracker.LCG.LcgTester;
 import me.cortex.TreeCracker.postProcessor.PostProcessor;
 import me.cortex.TreeCracker.program.CudaProgram;
@@ -15,40 +18,23 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         /*
-        LcgTester tester = new LcgTester();
-        CudaProgram cp = new CudaProgram();
-        cp.lcgVariableName = "seed";
-        cp.comparisonFailedReturn = "continue";
+        //Known 1.16.4 testing data
+        ChunkRand chunkRand = new ChunkRand();
+        long seed = chunkRand.setPopulationSeed(1234,-34 * 16,43 * 16, MCVersion.v1_16_4);
+        long decoSeed = chunkRand.setDecoratorSeed(seed, 1, 8, MCVersion.v1_16_4);
 
-        tester.nextBoolean().equalTo(true);
-        tester.nextFloat().greaterThan(0.543f);
-        tester.nextInt(69).lessThan(1);
-        tester.nextInt(69).lessThan(1);
-        tester.nextInt(69).lessThan(1);
-
-        System.out.println(tester.getFilterPower());
-        System.out.println(cp.assembleLcgTester(tester));
-
-        Random r = new Random(1358879 ^0x5DEECE66DL);
-        System.out.println(r.nextBoolean());
-        System.out.println(r.nextFloat()>0.543f);
-        System.out.println(r.nextInt(69)<1);
-        System.out.println(r.nextInt(69)<1);
-        System.out.println(r.nextInt(69)<1);
-        */
-
-
+        JRand rand = JRand.ofScrambledSeed(decoSeed);
+        System.out.println(rand.getSeed());
 
         TreeCrackerProgram program = new TreeCrackerProgram(
                 new Simple116BlobTree(Simple116BlobTree.FOREST_OAK_TREE, new TreePos(0,13), 6, new int[]{1,0,0,1,  0,1,1,1, 1,1,1,1}),
                 new Simple116BlobTree(Simple116BlobTree.FOREST_BIRCH_TREE, new TreePos(4,12), 5, new int[]{0,1,0,1,  0,-1,1,0, 1,0,0,0}),
                 new Simple116BlobTree(Simple116BlobTree.FOREST_OAK_TREE, new TreePos(12,9), 6, new int[]{1,1,1,1,  1,1,1,0,  1,1,1,1})
-
         );
+        */
 
-        program = new TreeCrackerProgram(
+        TreeCrackerProgram program = new TreeCrackerProgram(
                 new Simple112BlobTree(Simple112BlobTree.FOREST_OAK_TREE, new TreePos(7,11), 4, new int[]{-1,-1,1,-1, -1,-1,0,-1,  -1,-1,-1,-1}),
                 new Simple112BlobTree(Simple112BlobTree.FOREST_OAK_TREE, new TreePos(13,10), 5, new int[]{0,-1,1,-1,  0,-1,1,-1, -1,-1,-1,-1}),
 
