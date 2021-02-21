fbeb11be4e0b18cea06319c07bae1e04c0086b0a
mcrcortex <18544518+MCRcortex@users.noreply.github.com>
2021-02-21 20:31:45 +1000

    Worked on opencl

--- src/main/java/me/cortex/TreeCracker/program/opencl/OpenCLContext.java
+++ src/main/java/me/cortex/TreeCracker/program/opencl/OpenCLContext.java
@@ -1,4 +1,27 @@
 package me.cortex.TreeCracker.program.opencl;
 
+import org.jocl.cl_command_queue;
+import org.jocl.cl_context;
+import org.jocl.cl_device_id;
+import org.jocl.cl_queue_properties;
+
+import static org.jocl.CL.clReleaseCommandQueue;
+import static org.jocl.CL.clReleaseContext;
+
 public class OpenCLContext {
+    public final cl_context context;
+    public final cl_device_id device;
+    public final cl_queue_properties properties;
+    public final cl_command_queue commandQueue;
+    public OpenCLContext(cl_context context, cl_device_id device, cl_queue_properties properties, cl_command_queue commandQueue) {
+        this.context = context;
+        this.device = device;
+        this.properties = properties;
+        this.commandQueue = commandQueue;
+    }
+
+    public void release() {
+        clReleaseCommandQueue(commandQueue);
+        clReleaseContext(context);
+    }
 }
---
+++ src/main/java/me/cortex/TreeCracker/program/opencl/OpenCLSyncableMemory.java
@@ -0,0 +1,83 @@
+package me.cortex.TreeCracker.program.opencl;
+
+import org.jocl.Pointer;
+import org.jocl.Sizeof;
+import org.jocl.cl_mem;
+import sun.nio.ch.DirectBuffer;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+
+import static org.jocl.CL.*;
+
+public class OpenCLSyncableMemory {
+    private cl_mem gpu_memory;
+    private Buffer cpu_memory;
+    private int size_of_type;
+
+    private final OpenCLContext context;
+
+
+    public OpenCLSyncableMemory(OpenCLContext context, int size_of_type, Buffer buffer) {
+        if (buffer.capacity() % size_of_type != 0)
+            throw new IllegalArgumentException("Buffer capacity couldnt contain size of element");
+        this.size_of_type = size_of_type;
+        this.cpu_memory = buffer;
+        this.context = context;
+        this.gpu_memory =  clCreateBuffer(context.context, CL_MEM_READ_WRITE, buffer.capacity(), Pointer.to(buffer), null);
+    }
+
+    public OpenCLSyncableMemory(OpenCLContext context, int size_of_type, int number_of_elements) {
+        this(context, size_of_type, ByteBuffer.allocateDirect(size_of_type * number_of_elements));
+    }
+
+    public OpenCLSyncableMemory(OpenCLContext context, byte[] array) {
+        this(context, 1, ByteBuffer.wrap(array));
+    }
+
+    public OpenCLSyncableMemory(OpenCLContext context, int[] array) {
+        this(context, 4, IntBuffer.wrap(array));
+    }
+
+    public OpenCLSyncableMemory(OpenCLContext context, long[] array) {
+        this(context, 8, LongBuffer.wrap(array));
+    }
+
+
+
+
+    public void synchronizeToHost() {
+        this.synchronizeToHost(cpu_memory.capacity() / size_of_type);
+    }
+    public void synchronizeToGPU() {
+        this.synchronizeToGPU(cpu_memory.capacity() / size_of_type);
+    }
+
+    public void synchronizeToHost(int count) {
+        this.synchronizeToHost(count, 0);
+    }
+    public void synchronizeToGPU(int count) {
+        this.synchronizeToGPU(count, 0);
+    }
+
+
+
+    public int synchronizeToHost(int count, int offset) {
+        return  clEnqueueReadBuffer(context.commandQueue, gpu_memory, CL_TRUE, offset,
+                (long) count * size_of_type, Pointer.toBuffer(cpu_memory),
+                0, null, null);
+    }
+
+    public int synchronizeToGPU(int count, int offset) {
+        return clEnqueueWriteBuffer(context.commandQueue, gpu_memory, CL_TRUE, offset,
+                (long) count * size_of_type, Pointer.toBuffer(cpu_memory),
+                0, null, null);
+    }
+
+
+    public void release() {
+        clReleaseMemObject(gpu_memory);
+    }
+}
